0ca487c4a45cc1101b7844ac4c62385f9c8bd993
ambarbouzonn <ambarbouzon@gmail.com>
2024-04-11 09:46:18 -0300

    Ejemplo1.oy

---
+++ ejemplo1.py
@@ -0,0 +1,15 @@
+################################ DESARROLLO FUNCION #####################################
+
+def sumar_1():
+    primer_numero = input("Ingrese un numero: ")
+    primer_numero = int(primer_numero)
+    segundo_numero = input("Ingrese un segundo numero: ")
+    segundo_numero = int(segundo_numero)
+
+    suma = primer_numero + segundo_numero
+
+    print(f"La suma es: {suma}")
+
+################################ LLAMADA DE LA FUNCION ###################################
+
+sumar_1()
