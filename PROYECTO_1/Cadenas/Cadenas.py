ce5d0d2f4288c810826dfb1062a4a69a14be1723
ambarbouzonn <ambarbouzon@gmail.com>
2024-05-18 16:06:22 -0300

    Cadenas

--- PROYECTO_1/Cadenas/Cadenas.py
+++ PROYECTO_1/Cadenas/Cadenas.py
@@ -10,9 +10,9 @@
 # “o” 1
 # “u” 2
 
-# cadena = input("Ingrese una cadena: ").lower()
 
 def contador_vocales(cadena: str):
+    cadena = cadena.lower()
 
     matriz_vocales = [["a", 0],
                       ["e", 0],
@@ -37,15 +37,17 @@ def contador_vocales(cadena: str):
 # Crear una función que reciba una cadena y un caracter. La función deberá devolver 
 # el índice en el que se encuentre la primera incidencia de dicho caracter, o -1 en caso de que no esté.
 
-# cadena = input("Ingrese una cadena: ").lower()
-# caracter = input("Ingrese un caracter: ").lower()
 
-def recibir_cadena():
+def recibir_cadena(cadena: str, caracter: str):
+    cadena = cadena.lower()
+
     for i in range(len(cadena)):
         if caracter == cadena[i]:
-            return f"Caracter {caracter}, encontrado en el primer indice de {i+1}" 
-        
-    return -1
+            resultado = f"Caracter {caracter}, encontrado en el primer indice de {i+1}" 
+        else:
+            resultado -1
+
+    return resultado
 
 # resultado = recibir_cadena()
 # print(resultado)
@@ -56,15 +58,14 @@ def recibir_cadena():
 # Crear una función que reciba como parámetro una cadena y determine si la misma es 
 # o no un palíndromo. Deberá retornar un valor booleano indicando lo sucedido.
 
-# cadena = input("Ingrese una cadena: ").lower()
-
-def cadena_palindromo():
+def cadena_palindromo(cadena: str):
+    cadena = cadena.lower()
+    
     if cadena == cadena[::-1]:
         return True
     else:
         return False
     
-
 # resultado = cadena_palindromo()
 # print(resultado)
 
@@ -74,10 +75,85 @@ def cadena_palindromo():
 # Crear una función que reciba como parámetro una cadena y suprima los caracteres repetidos.
 # Ej: Si recibe como parámetro la cadena “Hooola” debe devolver “Hola”.
 
-cadena = input("Ingrese una cadena: ").lower()
+def eliminar_caracteres_repetidos(cadena: str):
+    cadena_sin_repetidos = ""
+    cadena = cadena.lower()
 
-def eliminar_caracteres_repetidos():
-    for i in range(len(cadena)):
-        caracter = cadena[i]
-        if caracter == i:
-            caracter
+    for caracter in cadena:
+        if caracter not in cadena_sin_repetidos:
+            cadena_sin_repetidos += caracter
+    return cadena_sin_repetidos
+
+# resultado = eliminar_caracteres_repetidos("Hoooola")
+# print(resultado)
+
+
+
+###################### 5
+# Crear una función que reciba una cadena por parámetro y suprima las vocales de la misma.
+# Ej: Si recibe como parámetro la cadena “Hola” debe devolver “Hl”
+
+
+def eliminar_vocales(cadena: str):
+    cadena_sin_vocales = ""
+    cadena = cadena.lower()
+
+    for caracter in cadena:
+        if caracter not in "aeiou":
+            cadena_sin_vocales += caracter
+        
+    return cadena_sin_vocales
+
+
+# resultado = eliminar_vocales("Hola")
+# print(resultado)
+
+
+
+###################### 6
+# Crear una función para contar cuántas veces aparece una subcadena dentro de una cadena.
+# Ej: Si recibe la cadena “El pan del panadero” y la subcadena “pan” deberá retornar el valor 2.
+
+def hallar_subcadena(cadena: str, sub_cadena: str):
+    cadena = cadena.lower()
+    sub_cadena = sub_cadena.lower()
+    contador = 0
+
+    sub_cadena_len = len(sub_cadena)
+    limite_cadena = len(cadena) - sub_cadena_len + 1
+
+    for i in range(limite_cadena):
+        if cadena[i:i + sub_cadena_len] == sub_cadena:
+            contador += 1
+
+    return contador
+
+
+# resultado = hallar_subcadena("Hay varias manzanas en el arbol de manzanas", "manzanas")
+# print(resultado)
+            
+
+
+###################### CSI UTN
+
+def encontrar_sospechoso():
+
+    numero_sospechosos = int(input("Ingrese la cantidad de sospechosos: "))
+
+    adn_encontrado = input("Ingrese la muestra de adn encontrada: ")
+
+    lista_sospechosos = {}
+
+    for _ in range(numero_sospechosos):
+        nombre_sospechoso = input("Ingrese el nombre del sospechoso: ")
+        adn_sospechoso = input("Ingrese el adn del sospechoso: ")
+        lista_sospechosos[nombre_sospechoso] = adn_sospechoso
+
+    for nombre_sospechoso in lista_sospechosos:
+        if adn_encontrado in lista_sospechosos[nombre_sospechoso]:
+            return nombre_sospechoso
+    return "Son todos inocentes"
+
+
+resultado = encontrar_sospechoso()
+print(resultado)
--- PROYECTO_1/Preguntados/juego_gui.py
+++ PROYECTO_1/Preguntados/juego_gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, font
 from juego import JuegoPreguntados
 import random
 
@@ -7,14 +7,15 @@ class JuegoGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Preguntados")
+        self.root.geometry("400x300")
         self.juego = JuegoPreguntados()
-        self.pregunta_label = tk.Label(root, text="")
-        self.pregunta_label.pack()
-        self.respuesta_var = tk.StringVar()
-        self.respuesta_entry = tk.Entry(root, textvariable=self.respuesta_var)
-        self.respuesta_entry.pack()
-        self.respuesta_button = tk.Button(root, text="Responder", command=self.responder)
-        self.respuesta_button.pack()
+        self.font = font.Font(family="Helvetica", size=12)
+        self.colores = {"Arte": "red", "Historia": "yellow", "Geografía": "blue", "Química": "green"}
+        self.pregunta_label = tk.Label(root, text="", font=self.font, wraplength=380, justify="center")
+        self.pregunta_label.pack(pady=10)
+        self.respuestas_frame = tk.Frame(root)
+        self.respuestas_frame.pack(pady=10)
+        self.respuesta_var = tk.StringVar()  # Se crea la variable de respuesta
         self.root.protocol("WM_DELETE_WINDOW", self.mostrar_puntos)
         self.siguiente_pregunta()
 
@@ -34,7 +35,19 @@ class JuegoGUI:
 
     def siguiente_pregunta(self):
         self.pregunta = self.juego.obtener_pregunta()
-        self.pregunta_label.config(text=f"{self.pregunta['pregunta']}\n\n{''.join([f'{i+1}. {opcion}\n' for i, opcion in enumerate(self.pregunta['opciones'])])}")
+        categoria = self.pregunta['categoria']
+        self.pregunta_label.config(text=f"{self.pregunta['pregunta']}\n", fg="black", bg=self.colores.get(categoria, "white"))
+        for widget in self.respuestas_frame.winfo_children():
+            widget.destroy()  # Limpiar las opciones de respuesta anteriores
+        # Mostrar opciones de respuesta
+        for i, opcion in enumerate(self.pregunta['opciones']):
+            respuesta_btn = tk.Button(self.respuestas_frame, text=f"{i+1}. {opcion}", font=self.font, bg=self.colores.get(categoria, "white"), fg="black", command=lambda opcion=opcion: self.responder_opcion(opcion))
+            respuesta_btn.pack(side=tk.TOP, padx=10, pady=5)
+
+    def responder_opcion(self, opcion):
+        respuesta_index = self.pregunta['opciones'].index(opcion) + 1
+        self.respuesta_var.set(str(respuesta_index))
+        self.responder()
 
     def mostrar_puntos(self):
         puntos_por_categoria = self.juego.puntos_por_categoria
